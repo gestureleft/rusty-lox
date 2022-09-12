9b29c95e1fc2938b8b61898f84c200594c831aa5
Harrison Marshall <hmar0007@student.monash.edu>
2022-09-13 09:52:16 +1000

    The repl now accepts input from the user

--- src/repl.rs
+++ src/repl.rs
@@ -1,5 +1,18 @@
+use std::io::{self, stdout, Write};
+
 use crate::error::Error;
 
 pub fn run_repl() -> Result<(), Error> {
-    unimplemented!("The REPL hasn't been implemented yet")
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    loop {
+        print!("> ");
+        stdout().flush()?;
+        stdin.read_line(&mut buffer)?;
+        if buffer == *"\n" {
+            break;
+        };
+        buffer.clear();
+    }
+    Ok(())
 }
